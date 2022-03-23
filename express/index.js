82c3dfc7131a4c88fa7808641fe5ee6456a25371
IvanJimenoRamirez <87144040+IvanJimenoRamirez@users.noreply.github.com>
2022-03-23 11:15:53 +0100

    task #13: Define database queries

--- express/index.js
+++ express/index.js
@@ -28,10 +28,6 @@ function db_connect() {
 app.use(bodyParser.json({ type: "application/json", limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-app.set('view engine', 'ejs');
-
-// Routes related to serving html/css/js public resources
-app.use(require("./routes/views.js"));
 
 app.use("/v1/", function(req, res, next) {
     const contentType = req.headers["content-type"];
@@ -49,5 +45,4 @@ function listen() {
     console.log("Express app started on port " + process.env.PORT);
 }
 
-listen();
-//db_connect();
+db_connect();
