4dbbd444000661061d8919b4850a5da9265b70e6
Francesc Holly <2012.1c.holfrancesc@gmail.com>
2022-04-07 09:27:55 +0200

    task #80: Define UserCtrl contracts

--- app/domain/User.js
+++ app/domain/User.js
@@ -1,14 +1,29 @@
 class User {
     
-    constructor(username, password) {
+    constructor(params) {
+        if (!'createdAt' in params) { return constructDefault(params.username, params.password) }
+        this.username = params.username;
+        this.password = params.password;
+        this.createdAt = params.createdAt;
+        this.karma = params.karma;
+        this.about = params.about;
+        this.email = params.email;
+        this.showdead = params.showdead;
+        this.noprocrast = params.noprocrast;
+        this.maxvisit = params.maxvisit;
+        this.minaway = params.minaway;
+        this.delay = params.delay;
+    }
+
+    constructDefault(username, password) {
         this.username = username;
         this.password = password;
-        this.createdAt = Date;
+        this.createdAt = Date.now();
         this.karma = 1;
-        this.about = [];
-        this.email = [];
-        this.showdead = Boolean;
-        this.noprocrast = Boolean;
+        this.about = "";
+        this.email = "";
+        this.showdead = false;
+        this.noprocrast = false;
         this.maxvisit = 20;
         this.minaway = 180;
         this.delay = 0;
