7b79c88eef7e2c3675ede478f334cdc3d3912b93
Francesc Holly <2012.1c.holfrancesc@gmail.com>
2022-05-05 09:14:35 +0200

    Try api auth on endpoint profile

--- app/api/users.js
+++ app/api/users.js
@@ -8,3 +8,17 @@ module.exports = router;
 
 const user_ctrl = new UserCtrl();
 const auth = new AuthMiddleware();
+
+router.get("/", auth.strict, async (req, res) => {
+    if (!req.query || !req.query.id) {
+        res.redirect("/user?id=" + req.user_auth_id);
+        return;
+    }
+    try {
+        let auth_id = req.user_auth_id;
+        let user = await user_ctrl.profile(auth_id, req.query.id);
+        res.status(200).json(user);
+    } catch {
+        res.json({"error_msg": "No such user"});
+    }
+});
