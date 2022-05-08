ee23887229883002cbd9cd27d0bd71a5a188ee97
Francesc Holly <2012.1c.holfrancesc@gmail.com>
2022-05-08 16:44:00 +0200

    Add user profile endpoints, add comment on submission endpoint, update auth middleware

--- app/api/submissions.js
+++ app/api/submissions.js
@@ -8,3 +8,19 @@ module.exports = router;
 const sub_ctrl = new SubmissionCtrl();
 const comm_ctrl = new CommentCtrl();
 const auth = new AuthMiddleware();
+
+router.post("/:id/comments", auth.strict, async (req, res) => {
+    const {text} = req.body;
+
+    if (!text || text === "") { 
+        res.status(400).json({"error_msg": "No 'text' field in body or it is empty"});
+        return; 
+    }
+
+    try {
+        let db_comment = await comm_ctrl.postComment(req.params.id, text, req.user_auth.id, req.user_auth.username);
+        res.status(201).json(db_comment);
+    } catch (e) {
+        res.status(404).json({"error_msg": "No such submission"});
+    }
+});
--- app/api/users.js
+++ app/api/users.js
@@ -9,16 +9,46 @@ module.exports = router;
 const user_ctrl = new UserCtrl();
 const auth = new AuthMiddleware();
 
-router.get("/", auth.strict, async (req, res) => {
-    if (!req.query || !req.query.id) {
-        res.redirect("/user?id=" + req.user_auth_id);
+router.get("/:id", auth.strict, async (req, res) => {
+    try {
+        let user = await user_ctrl.profile(req.user_auth.id, req.params.id);
+        res.status(200).json(user);
+    } catch {
+        res.status(404).json({"error_msg": "No such user"});
+    }
+});
+
+router.put("/:id", auth.strict, async (req, res) => {
+    if (req.params.id !== req.user_auth.id) {
+        res.status(403).json({"error_msg": "Only the owner of the account can update its profile"});
+        return;
+    }
+
+    let {about, showdead, noprocrast, maxvisit, minaway, delay} = req.body;
+    if (!about || !showdead || !noprocrast || !maxvisit || !minaway || !delay) {
+        res.status(400).json({"error_msg": "Form missing values. Must contain: [about, showdead, noprocrast, maxvisit, minaway, delay]"});
         return;
     }
     try {
-        let auth_id = req.user_auth_id;
-        let user = await user_ctrl.profile(auth_id, req.query.id);
+        showdead = showdead === "yes";
+        noprocrast = noprocrast === "yes";
+        maxvisit = parseInt(maxvisit);
+        minaway = parseInt(minaway);
+        delay = parseInt(delay);
+    } catch (e) {
+        res.status(400).json({"error_msg": "Form values are not in correct format"});
+        return;
+    }
+
+    if (maxvisit < 0 || minaway < 0 || delay < 0) {
+        res.status(409).json({"error_msg": "Form integers must be greater or equal than zero"});
+        return;
+    }
+
+    try {
+        let user = await user_ctrl.update(req.params.id, about, showdead, noprocrast, maxvisit, minaway, delay);
         res.status(200).json(user);
-    } catch {
-        res.json({"error_msg": "No such user"});
+    } catch (e) {
+        res.status(500).json({"error_msg": e.message});
     }
 });
