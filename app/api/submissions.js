02b9538ce6bdd0075a2538c3345e7e3996966c36
juliahc <julia.herrera.caba@estudiantat.upc.edu>
2022-05-09 21:09:41 +0200

    task #98 Submit new submission

--- app/api/submissions.js
+++ app/api/submissions.js
@@ -8,3 +8,19 @@ module.exports = router;
 const sub_ctrl = new SubmissionCtrl();
 const comm_ctrl = new CommentCtrl();
 const auth = new AuthMiddleware();
+
+router.post("/", auth.strict, async (req, res) => {
+    const {title, url, text} = req.body;
+    
+    if (title === "") {
+        res.status(400).json({"error_msg": "No 'title' field in body or it is empty"});
+        return;
+    }
+
+    try {
+        let db_submission =  await sub_ctrl.createSubmission(title, url, text, req.user_auth.id, req.user_auth.username);
+        res.status(201).json(db_submission);
+    } catch (e) {
+        res.status(400).json({"error_msg": "Error"});
+    }
+});
