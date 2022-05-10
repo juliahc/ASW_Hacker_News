f4fb887b68c9084d89c32b3ac5401efe231a2ef4
juliahc <julia.herrera.caba@estudiantat.upc.edu>
2022-05-10 18:25:31 +0200

    task #97 Get submissions by query

--- app/api/submissions.js
+++ app/api/submissions.js
@@ -9,6 +9,32 @@ const sub_ctrl = new SubmissionCtrl();
 const comm_ctrl = new CommentCtrl();
 const auth = new AuthMiddleware();
 
+router.get("/", auth.passthrough, async (req, res) => {
+
+    const {type, order, limit, offset} = req.query;
+
+    if(!type || !order || !limit || !offset) {
+        res.status(400).json({"error_msg": "Parameters missing in query. Must contain [type, order, limit, offset]"});
+        return;
+    }
+
+    if (!(type==="all" || type==="ask" || type==="url") || !(order==="pts" || order==="new") || limit<0 || offset<0 ) {
+        res.status(400).json({"error_msg": "Incorrect parameter(s) format. Must be [type={'all','ask','url'}, order={'pts','new'}, limit>=0, offset>=0]"});
+        return;
+    }
+
+    try {
+        let auth_id = req.user_auth !== null ? req.user_auth.id : null;
+        let sub_page = await sub_ctrl.fetchSubmissionsForParams(limit, offset,type,order,null,auth_id,auth_id);
+        sub_page.pop();
+        sub_page.forEach(submission => submission.formatCreatedAtAsTimeAgo());
+        res.status(200).json({sub_page});
+    } catch (e) {
+        res.status(500).json({"error_msg": e.message});
+    }
+});
+
+
 router.post("/", auth.strict, async (req, res) => {
 
     const {title, url, text} = req.body;
@@ -22,25 +48,25 @@ router.post("/", auth.strict, async (req, res) => {
         let db_submission =  await sub_ctrl.createSubmission(title, url, text, req.user_auth.id, req.user_auth.username);
         res.status(201).json(db_submission);
     } catch (e) {
-        res.status(404).json({"error_msg": "Error"});
+        res.status(500).json({"error_msg": e.message});
     }
 });
 
 router.get("/user/:id", auth.passthrough, async (req, res) => {   
 
-    if (!req.query.p || req.query.p < 1) {
-        res.status(400).json({"error_msg": "No 'page' param in request or it is < 1"});
+    const {limit, offset} = req.query;
+
+    if (!limit || !offset || limit < 0 || offset < 0) {
+        res.status(400).json({"error_msg": "No 'limit' or 'offset' param in request or they are < 1"});
     }
 
     try {
         let auth_id = req.user_auth !== null ? req.user_auth.id : null;
-        let p = 1;
-        if (req.query && req.query.p) p = req.query.p;
-        let sub_page = await sub_ctrl.fetchSubmissionsForParams(p,"any","new",req.params.id,auth_id);
+        let sub_page = await sub_ctrl.fetchSubmissionsForParams(limit, offset,"any","new",req.params.id, auth_id);
         sub_page.pop();
         sub_page.forEach(submission => submission.formatCreatedAtAsTimeAgo());
         res.status(200).json({sub_page});
-    } catch {
-        res.status(401).json({"error_msg": "No such user"});
+    } catch (e) {
+        res.status(500).json({"error_msg": "No such user"});
     }
 });
